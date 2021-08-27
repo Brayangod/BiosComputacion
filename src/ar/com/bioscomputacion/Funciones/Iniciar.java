b156d7fc19ea157ae03c1e597b81f51bd8cff71d
Brayan Godoy <bggodoy@ulp.edu.ar>
2021-08-27 14:20:07 -0300

    Se creo estructura inicial y login

---
+++ src/ar/com/bioscomputacion/Funciones/Iniciar.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.com.bioscomputacion.Funciones;
+
+import java.sql.*;
+
+/**
+ *
+ * @author braya
+ */
+public class Iniciar {
+    ConexionBD mysql = new ConexionBD();
+    Connection cn = mysql.getConexionBD();
+    public int verificarUsuario(String usuario, String contraseña){
+//        0 = No se pudo conecta a la base de datos
+//        1 = Se encontro usuario y contraseña
+//        2 = No se encontro ningun usuario y contraseña
+        try {
+            if(cn == null){
+                return 0;
+            }
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM usuario WHERE login='"+usuario+"' AND password='"+contraseña+"'");
+            
+            while(rs.next()){
+                return 1;
+            }
+        } catch (Exception e) {
+        }
+        return 2;
+    }
+}
